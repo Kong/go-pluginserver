eae0b80d5ffc5b609db41bbdbad64af75e117568
Javier Guerra <javier.guerra@konghq.com>
2019-11-18 10:31:25 -0500

    split for readability

---
+++ main.go
@@ -0,0 +1,43 @@
+// go-pluginserver is a standalone RPC server that runs
+// Go plugins for Kong.
+package main
+
+import (
+	"flag"
+	"github.com/ugorji/go/codec"
+	"log"
+	"net"
+	"net/rpc"
+)
+
+var socket = flag.String("socket", "", "Socket to listen into")
+
+func runServer(listener net.Listener) {
+	var handle codec.MsgpackHandle
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("accept(): %s", err)
+			return
+		}
+		rpcCodec := codec.MsgpackSpecRpc.ServerCodec(conn, &handle)
+		rpc.ServeCodec(rpcCodec)
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if *socket != "" {
+		listener, err := net.Listen("unix", *socket)
+		if err != nil {
+			log.Printf(`listen("%s"): %s`, socket, err)
+			return
+		}
+
+		rpc.RegisterName("plugin", newServer())
+
+		runServer(listener)
+	}
+}
